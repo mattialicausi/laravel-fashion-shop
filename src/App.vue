f6ce9d6deea08de61bf6f1206d319577f57cddf9
mattialicausi <licausivitomattia@gmail.com>
2023-01-25 00:17:35 +0100

    aggiunto breadcrumb component in app.vue, da inserire tutte le rotte in crumbsLinks

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@
     
 
       <main>
+        <div class="container mt-5">
+          <BreadCrumbComponent class=" row align-items-center" :crumbs= "crumbsLinks" @selected= "selected"/>
+        </div>
+    
        <router-view></router-view>
     </main>
 
@@ -24,11 +28,40 @@
 import FooterComponent from './components/FooterComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import NavBarComponent from './components/NavBarComponent.vue';
-
+import BreadCrumbComponent from './components/BreadCrumbComponent.vue';
 
 
   export default {
-    components: { HeaderComponent, NavBarComponent, FooterComponent }
+    components: { HeaderComponent, NavBarComponent, FooterComponent, BreadCrumbComponent },
+
+    data () {
+      return {
+
+        crumbsLinks: [
+                  {
+                      label: 'Home',
+                      routeName: 'home'
+                  },
+                  {
+                      label: 'Products',
+                      routeName: 'category'
+                  },
+                  // {
+                  //     label: 'Product',
+                  //     routeName: 'single-product'
+                  // },
+                   
+        ],
+
+      }
+    },
+    methods: {
+
+      selected(crumb) {
+        console.log(crumb)
+      }
+
+    },
 }
 </script>
 
