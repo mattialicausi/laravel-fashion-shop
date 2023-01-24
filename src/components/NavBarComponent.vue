ee3fe7af2002fef2718c82796341b3e771a5e048
Francescolapiana <Lushika1219@gmail.com>
2023-01-24 18:57:44 +0100

    footer done

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -3,13 +3,20 @@
         <div class="navcontent">
             <div class="container">
                 <ul class="container">
-                    <li><a href="">rossetti</a></li>
+                    <li>
+                        <a href="">rossetti</a>
+                        <!-- <div class="dropdownn">
+                            <a href="" class="lidropdownn">Brand1</a>
+                            <a href="" class="lidropdownn">Brand2</a>
+                            <a href="" class="lidropdownn">Brand3</a>
+                        </div> -->
+                    </li>
                     <li><a href="">eyeshadow</a></li>
                     <li><a href="">bronzer</a></li>
                     <li><a href="">blush</a></li>
                     <li><a href="">mascara</a></li>
                     <li><a href="">fundation</a></li>
-                    <li><a href="">altri prodotti</a></li>
+                    <li><a href="">prodotti</a></li>
                     <li><a href="">contattaci</a></li>
                 </ul>
             </div>
@@ -51,9 +58,15 @@
                 align-items: center;
                 li{
                     list-style: none;
+                    position: relative;
+                    width: 100%;
+                    &:hover .dropdownn{
+                            display: block;
+                        }
                     a{
                         display: block;
                         text-transform: uppercase;
+                        text-align: center;
                         color: $grey;
                         text-decoration: none;
                         background-color: $mainColor;
@@ -64,6 +77,29 @@
                             text-shadow: 0px 0px 4px $white;
                             background-color: $mainColorActive;
                         }
+                        
+                    }
+                    .dropdownn{
+                    background-color: $mainColorActive;
+                    position: absolute;
+                    width: 100%;
+                    margin: 0;
+                    padding: 0;
+                    flex-direction: column;
+                    align-items: center;
+                    box-shadow: 0px 4px 4px $shadow;
+                    transition: all 250ms;
+                    display: none;
+                    z-index: 1;
+                            a{
+                                display: block;
+                                background-color: $mainColorActive;
+                                width: 100%;
+                                &:hover{
+                                    filter: brightness(1.1);
+                                }
+                            }
+                        
                     }
                 }
             }
