1b7f72fe03f0a48629de6a93c078c8f05cc5a4f3
GioGreco <grecogiorgio.97@gmail.com>
2023-01-26 17:36:05 +0100

    add some fx

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 import {router} from './router'
 
 import './assets/styles/main.scss'
 
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(MotionPlugin).mount('#app')
